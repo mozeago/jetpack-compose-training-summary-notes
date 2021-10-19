b1daba947510aa14378c41873607107c07b4ac20
Moses Asiago <mozzeago@gmail.com>
2021-10-19 08:15:02 +0300

    added material desing ton the application

--- app/src/main/java/com/mozeago/jetpackcomposelesson1/MainActivity.kt
+++ app/src/main/java/com/mozeago/jetpackcomposelesson1/MainActivity.kt
@@ -3,11 +3,19 @@ package com.mozeago.jetpackcomposelesson1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mozeago.jetpackcomposelesson1.ui.theme.JetpackComposeLesson1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +25,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeLesson1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MessageCard(Message("Moses Asiago", "Hello everyone"))
                 }
             }
         }
@@ -25,14 +33,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    JetpackComposeLesson1Theme {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.photo),
+                contentDescription = "Person profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(
+                        CircleShape
+                    )
+                    .border(1.5.dp, MaterialTheme.colors.secondaryVariant, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = msg.body, style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+
 }
+
+data class Message(val author: String, val body: String)
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeLesson1Theme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(Message("Priyanka", "Chao !"))
 }
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    JetpackComposeLesson1Theme {
+//        Greeting("Android")
+//    }
+//}
