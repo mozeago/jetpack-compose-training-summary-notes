ca3151d3a5687bba787d1d675f23f3fda9337e68
Moses Asiago <mozzeago@gmail.com>
2021-10-27 07:47:37 +0300

    added animation support for the application

--- app/src/main/java/com/mozeago/jetpackcomposelesson1/MainActivity.kt
+++ app/src/main/java/com/mozeago/jetpackcomposelesson1/MainActivity.kt
@@ -3,16 +3,22 @@ package com.mozeago.jetpackcomposelesson1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +31,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeLesson1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MessageCard(Message("Moses Asiago", "Hello everyone"))
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -47,14 +53,32 @@ fun MessageCard(msg: Message) {
                     .border(1.5.dp, MaterialTheme.colors.secondaryVariant, CircleShape)
             )
             Spacer(modifier = Modifier.width(8.dp))
-            Column {
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor: Color by animateColorAsState(
+                if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+            )
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
                 Text(
                     text = msg.author,
                     color = MaterialTheme.colors.secondaryVariant,
                     style = MaterialTheme.typography.subtitle2
                 )
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(text = msg.body, style = MaterialTheme.typography.body2)
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier
+                        .animateContentSize()
+                        .padding(1.dp)
+                ) {
+                    Text(
+                        text = msg.body, style = MaterialTheme.typography.body2,
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                    )
+                }
+
             }
         }
     }
@@ -63,20 +87,21 @@ fun MessageCard(msg: Message) {
 
 data class Message(val author: String, val body: String)
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message("Priyanka", "Chao !"))
+    JetpackComposeLesson1Theme() {
+        Conversation(SampleData.conversationSample)
+    }
 }
-//@Composable
-//fun Greeting(name: String) {
-//    Text(text = "Hello $name!")
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    JetpackComposeLesson1Theme {
-//        Greeting("Android")
-//    }
-//}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { msg ->
+            MessageCard(msg = msg)
+        }
+    }
+}
